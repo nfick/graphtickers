b4706d171e7491af8335df23923e14779672982b
Nathanael <nfickett@gmail.com>
2018-09-08 19:18:06 -0500

    Fixed gliches, broken css

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import pandas
 import simplejson as json
@@ -28,60 +29,76 @@ def graph():
         ticker= request.form['ticker']
         start_date = request.form['start_date']
         end_date = request.form['end_date']
-
-        api_url = '''https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?
-             api_key=XxmSiACUars8yTcqLQhX&
-             start_date=%s&end_date=%s''' % (ticker, start_date, end_date)
+        form_open = ''
+        adj_open = ''
+        high = ''
+        adj_high = ''
+        low = ''
+        adj_low = ''
+        close = ''
+        adj_close = ''
+        
+        api_url = '''https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?api_key=XxmSiACUars8yTcqLQhX&start_date=%s&end_date=%s''' % (ticker, start_date, end_date)
         session = requests.Session()
         session.mount('https://', requests.adapters.HTTPAdapter(max_retries=3))
         raw_data = session.get(api_url)
 
-        data = raw_data.json()
-        ticker_df = pandas.DataFrame(data['data'], columns=data['column_names'])
-        ticker_df ['Date'] = pandas.to_datetime(ticker_df ['Date'])
+        raw_data = raw_data.json()
+        ticker_df = pandas.DataFrame(raw_data['dataset_data']['data'], 
+            columns=raw_data['dataset_data']['column_names'])
+        ticker_df['Date'] = pandas.to_datetime(ticker_df['Date'])
 
-        plot = figure(tools=TOOLS,
-              title='Data from Quandle for %s' % ticker,
+        plot = figure(title='Data from Quandle for %s' % ticker,
               x_axis_label='date',
               x_axis_type='datetime')
 
-        if request.form.get('Open'):
+        if request.form.get('open'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Open'].values,
                 line_width=2, line_color=Category20[20][0], legend='Open')
-        if request.form.get('Adj. Open'):
+            form_open = 'checked'
+        if request.form.get('adj_open'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Adj. Open'].values,
                 line_width=2, line_color=Category20[20][1], legend='Adj. Open')
-         if request.form.get('High'):
+            adj_open = 'checked'
+        if request.form.get('high'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['High'].values,
                 line_width=2, line_color=Category20[20][2], legend='High')
-        if request.form.get('Adj. High'):
+            high = 'checked'
+        if request.form.get('adj_high'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Adj. High'].values,
                 line_width=2, line_color=Category20[20][3], legend='Adj. High')
-        if request.form.get('Low'):
+            adj_high = 'checked'
+        if request.form.get('low'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Low'].values,
                 line_width=2, line_color=Category20[20][4], legend='Low')
-        if request.form.get('Adj. Low'):
+            low = 'checked'
+        if request.form.get('adj_low'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Adj. Low'].values,
                 line_width=2, line_color=Category20[20][5], legend='Adj. Low')
-        if request.form.get('Close'):
+            adj_low = 'checked'
+        if request.form.get('close'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Close'].values,
-                line_width=2, line_color=Category20[20][6], legend='Close')
-        if request.form.get('Adj. Close'):
+                line_width=2, line_color=Category20[20][10], legend='Close')
+            close = 'checked'
+        if request.form.get('adj_close'):
             plot.line(x=ticker_df['Date'].values, 
                 y=ticker_df['Adj. Close'].values,
-                line_width=2, line_color=Category20[20][7], legend='Adj. Close')
+                line_width=2, line_color=Category20[20][11], legend='Adj. Close')
+            adj_close = 'checked'
         
 
         script, div = components(plot)
-        return render_template('graph.html', end=end_date, start=start_date,
-                               script=script, div=div)
+        return render_template('graph.html', ticker=ticker, 
+            end=end_date, start=start_date, script=script, div=div,
+            form_open=form_open, adj_open=adj_open, high=high, adj_high=adj_high,
+            low=low, adj_low=adj_low, close=close, adj_close=adj_close)
 
 if __name__ == '__main__':
     app.run(port=33507)
